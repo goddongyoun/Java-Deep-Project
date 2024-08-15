fdae0eedb5cce6b8547bacc268a7eea050581a0e
goddongyoun <80258364+goddongyoun@users.noreply.github.com>
2024-08-15 17:45:05 +0900

    Connection Test 만듬, Server가 실행 중일 때 Client가 Connection Test요청을 보내면 Server가 Connection Good을 반환함

--- testConnection/src/testConnection/UDPtestAsClient.java
+++ testConnection/src/testConnection/UDPtestAsClient.java
@@ -1,26 +1,44 @@
 package testConnection;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class UDPtestAsClient {
 
 	public static void main(String[] args) {
-		String s = "Send message";
+		String s = "Connection Test";
 		byte[] buf = s.getBytes();
+		byte[] recvBuf = new byte[256];
 		Scanner sc = new Scanner(System.in);
+		DatagramSocket socket;
 		try {
-			DatagramSocket socket = new DatagramSocket(4000); // 송/수신 포트를 지정. 여기서는 보낼 포트를 지정
-			InetAddress address = InetAddress.getByAddress(new byte[] {127, 0, 0, 1}); // 인터넷 주소를 저장 getByName으로 해도 상관없으나 효율성 위해 getByAddress로
-			DatagramPacket packet = new DatagramPacket(buf, buf.length,address,5000); // 보낼 준비, 파라미터는 순서대로 1. 보낼 바이트 배열, 2. 그 길이, 3. 보낼 위치, 4. 보낼 포트
+			socket = new DatagramSocket(4000); // 송/수신 포트를 지정. 여기서는 보낼 포트를 지정
+			InetAddress addr = InetAddress.getByAddress(new byte[] {127,0,0,1});
+			DatagramPacket packet = new DatagramPacket(buf, buf.length, addr, 5000); // 보낼 준비, 파라미터는 순서대로 1. 보낼 바이트 배열, 2. 그 길이, 3. 보낼 위치, 4. 보낼 포트
+			ExecutorService executorService = Executors.newFixedThreadPool(2);
+			executorService.execute(()->{
+				System.out.println("ExecutorService Init");
+				DatagramPacket recvPacket = new DatagramPacket(recvBuf, recvBuf.length);
+				try {
+					socket.receive(recvPacket);
+					System.out.println(new String(recvPacket.getData()).trim() + " Received" + recvPacket.getAddress() +" : "+recvPacket.getPort());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
 			socket.send(packet);
 			System.out.println("Sended");
+			System.out.println("Input string to send");
 			buf = sc.nextLine().getBytes();
 			packet.setData(buf);
 			socket.send(packet);
 			socket.close();
+			System.out.println("closed");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
--- testConnection/src/testConnection/UDPtestAsServer.java
+++ testConnection/src/testConnection/UDPtestAsServer.java
@@ -2,6 +2,7 @@ package testConnection;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class UDPtestAsServer {
 
@@ -11,10 +12,32 @@ public class UDPtestAsServer {
 			System.out.println("Server trying to start...");
 			DatagramSocket socket = new DatagramSocket(5000); // 송/수신 포트를 지정. 여기서는 수신용
 			DatagramPacket packet = new DatagramPacket(buf, buf.length); // 수신된 정보를 패킷에 저장할 바이트 변수 위치와 길이 알려주기
+			InetAddress addr = InetAddress.getByName("127.0.0.1");
+			DatagramPacket sendPacket = new DatagramPacket(buf, buf.length, addr, 4000);
+			
+			socket.setReuseAddress(true);
+			System.out.println(socket.getReuseAddress());
 			System.out.println("Server is receiving.");
+			StringBuffer sb = new StringBuffer();
+			StringBuffer compare = new StringBuffer();
 			while(!(buf.toString().equals("KILL SERVER"))) {
 				socket.receive(packet); // 수신 실행 (멀티 스레드가 아닌 이상 프로그램은 수신이 올 때까지 일시 중단)
-				System.out.println(new String(packet.getData())+", "+ packet.getAddress() +", "+ packet.getPort());
+				System.out.println(new String(packet.getData()).trim() + packet.getAddress() + "/" + packet.getPort());
+				sb.setLength(0);
+				sb.append(new String(packet.getData()).trim());
+				compare.setLength(0);
+				compare.append("Connection Test");
+				if(sb.compareTo(compare) == 0) {
+					for(int i = 0; i<buf.length; i++) {
+						buf[i] = 0;
+					}
+					sb.setLength(0);
+					buf = sb.append("ConnectionGood").toString().getBytes();
+					sendPacket.setData(buf);
+					socket.send(sendPacket);
+					System.out.println(new String(buf) + " Sended");
+				}
+				
 				for(int i = 0; i<buf.length; i++) {
 					buf[i] = 0;
 				}
