7502b6a93ea596ddbf42129f1f9cc347cfd66305
plmjuio1234 <47781445+plmjuio1234@users.noreply.github.com>
2024-09-03 10:23:36 +0900

    Add libGDX

---
+++ Projects/lwjgl3/build/resources/main/maps/Basic_tools_and_meterials.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Basic_tools_and_meterials" tilewidth="16" tileheight="16" tilecount="6" columns="3">
+ <image source="Basic_tools_and_meterials.png" width="48" height="32"/>
+</tileset>
---
+++ Projects/lwjgl3/build/resources/main/maps/Simple_Milk_and_grass_item.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Simple_Milk_and_grass_item" tilewidth="16" tileheight="16" tilecount="4" columns="4">
+ <image source="Simple_Milk_and_grass_item.png" width="64" height="16"/>
+</tileset>
